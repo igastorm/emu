704d4f4379074ee83f9c7925aeb36f286fd0046c
igastorm <167081895+igastorm@users.noreply.github.com>
2026-06-19 07:14:35 +0000

    Add Emulator class

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,57 @@
+#include <cstddef>
+#include <cstdint>
 #include <iostream>
 using std::cout;
 
-int main(int argc, char** argv) {
-  cout << "Hello from emu!\n";
+// 1MB
+constexpr size_t MEMORY_SIZE = 1024 * 1024;
+
+class Register {
+protected:
+  // Registers
+  enum : uint8_t {
+    EAX,
+    ECX,
+    EDX,
+    EBX,
+    ESP,
+    EBP,
+    ESI,
+    EDI,
+    EIP,
+    ELFLAGS,
+    REGISTERS_COUNT
+  };
+};
+
+class Emulator : private Register {
+private:
+  // Registers
+  uint32_t registers[REGISTERS_COUNT];
+
+  // memory
+  uint8_t *const memory;
+
+  // Init complete flag
+  bool ok;
+
+public:
+  Emulator(size_t, uint32_t, uint32_t) noexcept;
+  ~Emulator(void) noexcept { delete[] this->memory; }
+  bool is_ok(void) const noexcept { return this->ok; };
+};
+
+Emulator::Emulator(size_t size, uint32_t eip, uint32_t esp) noexcept
+    : registers{}, memory(new(std::nothrow) uint8_t[size]), ok(false) {
+  this->registers[ESP] = esp;
+  this->registers[EIP] = eip;
+  if (this->memory != nullptr) {
+    this->ok = true;
+  }
+}
+
+int main(int argc, char **argv) {
+  Emulator emulator(MEMORY_SIZE, 0x0000, 0x7c00);
+  cout << "Hello from emu\n";
   return 0;
 }
